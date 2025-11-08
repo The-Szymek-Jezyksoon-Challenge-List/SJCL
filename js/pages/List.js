3a4f49e82838ef94dfadce249081f35a9d27908f
lubiegry353 <szymekkurp121@gmail.com>
2025-11-08 17:10:55 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -90,7 +90,7 @@ export default {
                         <p class="error" v-for="error of errors">{{ error }}</p>
                     </div>
                     <div class="og">
-                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
+                        <p class="type-label-md">Strona stworzona przez <a href="https://tsl.pages.dev/" target="_blank">Szymka</a></p>
                     </div>
                     <template v-if="editors">
                         <h3>List Editors</h3>
