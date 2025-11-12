f96832d4cdd35a739851b712845c37e3a5268fde
lubiegry353 <szymonkurp3@gmail.com>
2025-11-12 15:08:06 +0100

    Update packs.js

--- js/pages/packs.js
+++ js/pages/packs.js
@@ -1,1 +1,8 @@
-
+export default {
+  template: `
+    <div style="color:white; text-align:center; padding:40px;">
+      <h1>Packs działa 🎉</h1>
+      <p>Jeśli to widzisz, to router działa poprawnie.</p>
+    </div>
+  `
+};
