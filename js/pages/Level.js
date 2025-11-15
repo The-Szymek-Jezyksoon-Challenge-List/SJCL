69c98f9f254b4d5bb93f5011c91411f88975566d
lubiegry353 <szymonkurp3@gmail.com>
2025-11-15 14:44:12 +0100

    Update Level.js

--- js/pages/Level.js
+++ js/pages/Level.js
@@ -1,1 +1,14 @@
+export default {
+  props: ['id'],
+  template: `
+    <section style="color:white; text-align:center; padding:40px;">
+      <h1>Level: {{ id }}</h1>
+      <p>Tu będą dane levela (nazwa, twórca, rekordy itp.)</p>
+
+      <router-link to="/packs" style="color:cyan;">
+        ⬅ Powrót do packów
+      </router-link>
+    </section>
+  `
+};
 
